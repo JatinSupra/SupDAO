6dc1f7d634b4316f467216b849c26fd906192763
JatinSupra <j.pandya@supraoracles.com>
2025-03-21 11:44:34 +0530

    fix for voting DAO error

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,9 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     supra: {
-      url: "https://rpc-evmstaging.supra.com/rpc/v1/eth",
+      url: "",
       chainId: 232,
-      accounts: [`0x3c6fc035d49d6ea4040019853068366bc16a24961054deb7b5d94d4b017fdb9e`]
+      accounts: [``]
     }
   }
 };
