21ab9d0133bd8463839597d8ed71cbd3e470320b
Jatin Pandya <j.pandya@supraoracles.com>
2025-10-29 20:37:01 +0530

    Refactor deployment script to use raw transaction

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,41 @@
 const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
+    let wallet;
+    const provider = ethers.provider;
+    const accounts = hre.network.config.accounts;
+
+    if (accounts.mnemonic) {
+        const hdNode = ethers.utils.HDNode.fromMnemonic(accounts.mnemonic);
+        const derivedNode = hdNode.derivePath(accounts.path + "/0");
+        wallet = new ethers.Wallet(derivedNode.privateKey, provider);
+    } else if (Array.isArray(accounts)) {
+        wallet = new ethers.Wallet(accounts[0], provider);
+    } else {
+        throw new Error("Unable to get private key from config");
+    }
+    
+    console.log("Deploying from:", wallet.address);
     const CrossChainDAO = await ethers.getContractFactory("CrossChainDAO");
-    const dao = await CrossChainDAO.deploy();
-    await dao.deployed();
-    console.log("DAO deployed to:", dao.address);
+    const deployTx = await CrossChainDAO.getDeployTransaction();
+        const tx = {
+        from: wallet.address,
+        data: deployTx.data,
+        gasLimit: await provider.estimateGas({
+            from: wallet.address,
+            data: deployTx.data
+        }),
+        gasPrice: await provider.getGasPrice(),
+        nonce: await provider.getTransactionCount(wallet.address),
+        chainId: (await provider.getNetwork()).chainId
+    };
+    const signedTx = await wallet.signTransaction(tx);
+    const txHash = await provider.send("eth_sendRawTransaction", [signedTx]);
+    console.log("Transaction hash (from RPC):", txHash);
+    const receipt = await provider.waitForTransaction(txHash);
+    console.log("DAO deployed to:", receipt.contractAddress);
+    console.log("Block number:", receipt.blockNumber);
 }
 
 main().catch((error) => {
