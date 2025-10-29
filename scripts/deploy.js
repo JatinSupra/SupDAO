4f6a3b0d07f3a42231c753b577661036786163b1
Jatin Pandya <j.pandya@supraoracles.com>
2025-10-29 21:03:23 +0530

    Refactor deploy script to simplify wallet creation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,40 +2,48 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
-    let wallet;
+    const privateKey = hre.network.config.accounts[0];
     const provider = ethers.provider;
-    const accounts = hre.network.config.accounts;
-
-    if (accounts.mnemonic) {
-        const hdNode = ethers.utils.HDNode.fromMnemonic(accounts.mnemonic);
-        const derivedNode = hdNode.derivePath(accounts.path + "/0");
-        wallet = new ethers.Wallet(derivedNode.privateKey, provider);
-    } else if (Array.isArray(accounts)) {
-        wallet = new ethers.Wallet(accounts[0], provider);
-    } else {
-        throw new Error("Unable to get private key from config");
-    }
-    
+    const wallet = new ethers.Wallet(privateKey, provider);
     console.log("Deploying from:", wallet.address);
-    const CrossChainDAO = await ethers.getContractFactory("CrossChainDAO");
-    const deployTx = await CrossChainDAO.getDeployTransaction();
-        const tx = {
-        from: wallet.address,
+    console.log("Chain ID:", hre.network.config.chainId);
+    const CrossChainDAO = await ethers.getContractFactory("CrossChainDAO", wallet);
+    const deployTx = CrossChainDAO.getDeployTransaction();
+    
+    const tx = {
         data: deployTx.data,
-        gasLimit: await provider.estimateGas({
-            from: wallet.address,
-            data: deployTx.data
-        }),
-        gasPrice: await provider.getGasPrice(),
+        chainId: hre.network.config.chainId,
         nonce: await provider.getTransactionCount(wallet.address),
-        chainId: (await provider.getNetwork()).chainId
+        gasLimit: ethers.BigNumber.from(5000000), // 5M gas
+        gasPrice: await provider.getGasPrice(),
+        type: 0 // Legacy transaction
     };
+    
+    console.log("Transaction:", {
+        nonce: tx.nonce,
+        gasLimit: tx.gasLimit.toString(),
+        gasPrice: tx.gasPrice.toString(),
+        chainId: tx.chainId
+    });
+    
+    // Sign the transaction
     const signedTx = await wallet.signTransaction(tx);
+    
+    console.log("\nSending raw transaction...");
+    
+    // Send raw transaction and get hash from SupraEVM RPC
     const txHash = await provider.send("eth_sendRawTransaction", [signedTx]);
-    console.log("Transaction hash (from RPC):", txHash);
+    
+    console.log("\nTransaction Hash (from SupraEVM):", txHash);
+    
+    // Wait for confirmation
+    console.log("\nWaiting for confirmation...");
     const receipt = await provider.waitForTransaction(txHash);
-    console.log("DAO deployed to:", receipt.contractAddress);
+    
+    console.log("\nDeployed to:", receipt.contractAddress);
     console.log("Block number:", receipt.blockNumber);
+    console.log("Gas used:", receipt.gasUsed.toString());
+    console.log("Status:", receipt.status === 1 ? "Success" : "Failed");
 }
 
 main().catch((error) => {
